13fa6375369d8cc43f4deea10dee0c25a5473dea
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 19:28:59 -0300

    Create Exericio4.py

---
+++ Exericio4.py
@@ -0,0 +1,6 @@
+#faça um programa que calcule um aumento de 15% para um salario de R$750
+x = int (input("Insira o salario:"))
+aumento = int (input("Insira a quantidade de aumento:"))
+y = (x/100)*aumento
+z = y + x
+print (z)
