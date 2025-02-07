3d3709ba450a2a3dea127f2bf0db373c54087168
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 20:29:29 -0300

    Create Exercicio9.py

---
+++ Exercicio9.py
@@ -0,0 +1,6 @@
+#escreva um programa que peça ao usuario para inserir três numeros inteiros, some os dois primeiro e multipplique esse total pelo terceiro, exiba o resultado da operação com a seguinte mensagem: "O total é: (:)"
+num1 = int (input("Insira o primeiro numero: "))
+num2 = int (input("Insira o segundo numero: "))
+num3 = int (input("Insira o terceiro numero: "))
+soma = num1+num2
+print ("O total é: ", soma*num3)
