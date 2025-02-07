c52700e001ebc7711b90dccb596f95567b22c988
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 19:46:38 -0300

    Create Exercicio7.py

---
+++ Exercicio7.py
@@ -0,0 +1,3 @@
+#Escreva um programa que pergunte "Qual u.c você mais gosta?" e na proxima linha a resposta "E gosto de programação de aplicativos"
+uc = input("Qual u.c você mais gosta?  ")
+print(uc, "e de programação de aplicativos")
