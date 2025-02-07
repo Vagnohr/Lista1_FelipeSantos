ce4fb93146fe330f42cff447420addd588fd5b59
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 19:56:15 -0300

    Update Exercicio8.py

---
+++ Exercicio8.py
@@ -0,0 +1,4 @@
+#escreva um programa que peça ao usiario para inserir dois numeros inteiros, some-os e exiba a mensagem: "O total é: (soma dos valores)"
+num1 = int (input("Insira o primeiro número: "))
+num2 = int (input("Insira o segundo número: "))
+print("O total é:", num1+num2)
