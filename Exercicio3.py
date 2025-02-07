491ef42fc65655f430068a3a19579313e26c676e
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 19:22:15 -0300

    Create Exercicio3.py

---
+++ Exercicio3.py
@@ -0,0 +1,5 @@
+#escreva um prograa que calcule a soma de três variáveis e imprima o resultado na tela
+x = int (input ("Insira a primeira variavel:"))
+y = int (input ("Insira a segunda variavel:"))
+z = int (input ("Insira a terceira variavel:"))
+print (x+y+z)
