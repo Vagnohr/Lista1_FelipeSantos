025f962568901259913ff722a26d0880b7a810f2
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 19:39:43 -0300

    Update Exercicio6.py

---
+++ Exercicio6.py
@@ -0,0 +1,4 @@
+#faça um programa que solicite o nome e em seguida o sobrenome do usuario e exiba a mensagem "Olá" + nome digitado + sobrenome digitado
+nome = input("Insira o seu nome:")
+sobrenome = input("Insira o seu sobrenome:")
+print("Olá", nome, sobrenome)
