e03b2d8b7aa9e13383779daab73ef58d88319aa5
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-07 21:17:33 -0300

    Update Exercicio13.py

--- Exercicio13.py
+++ Exercicio13.py
@@ -1,6 +1,6 @@
 #escreva um programa que solicite um determinado número de dias, em seguida exiba o quanto esses dias equivalem em horas, minutos e segundos
 dias = int(input("Insira quantidade de dias:"))
-horas = dias*60
+horas = dias*24
 minutos = horas*60
 segundos = minutos*60
 print("Horas:", horas, "minutos:", minutos, "segundos:", segundos)
